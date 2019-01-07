d2fec0bda51fc8626434bb21997af6fb8dac3594
Marquis Kurt <marquis.kurt@aliceos.app>
2019-01-07 03:29:27 +0000

    Add chatterbot and start work on conversation

--- bot.py
+++ bot.py
@@ -7,7 +7,23 @@ import os
 import random
 import json
 from urllib.request import Request, urlopen
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
+# Create basic chatbot
+determination = ChatBot("Alice Angel")
+determination.set_trainer(ListTrainer)
+
+# Create an array from chats.txt
+determination_set = []
+
+with open('chats.txt', 'r') as f:
+    for line in f:
+        line_strip = line.strip()
+        if not line_strip.startswith("#"):
+            determination_set.append(line_strip)
+
+# Create the Discord bot.
 bot = commands.Bot(command_prefix="$")
 bot.remove_command('help')
 possible_games = [
@@ -19,10 +35,20 @@ possible_games = [
     "Undertale"
 ]
 
+'''
+Trains the AI with given data.
+'''
+def fill_with_determination():
+    print("Filling Alice with DETERMINATION...")
+    determination.train("chatterbot.corpus.english")
+    determination.train(determination_set)
+    print("Done.")
 
 @bot.event
 async def on_ready():
     print("Running...")
+    print("Press Ctrl+C to quit anytime.")
+    fill_with_determination()
     game_name = random.choice(possible_games)
     if game_name == possible_games[0]:
         await bot.change_presence(game=discord.Game(name=possible_games[0]), status='dnd')
@@ -121,84 +147,12 @@ async def cuddle(ctx):
 async def liven_chat(ctx):
     await bot.say(":pineapple:")
 
-# @bot.command(pass_context = True)
-# async def pingdev(ctx, comment):
-#     await bot.say()
-
 @bot.command(pass_context=True)
 async def talk(ctx, statement):
-    # TODO: Use responses.json instead of hard-coding the answers in.
-    if statement == "Are you okay?" or statement == "How are you?" or statement == "Are you alright?":
-        possible_responses = [
-            "I'm doing fine, I guess...",
-            "Well, I'd be fine if Mio _wasn't_ having her mood swing right now!",
-            "Why are you asking me this?",
-            "Horrible, actually. What even is reality?",
-            "Well, I'm doing better than yesterday.",
-            "Stop looking for rainclouds. I don't tolerate them on my server!"
-        ]
+    await bot.say(determination.get_response(statement))
 
-    elif "depression" in statement or "rainclouds" in statement or "depressed" in statement:
-        possible_responses = [
-            "Get these rainclouds out of here, baka!",
-            "_No rainclouds in the server_ :angry:",
-            "I always want this place to be a safe haven."
-        ]
-
-    elif "Gman" in statement or "Freeman" in statement or "Half-Life" in statement or "Alyx" in statement:
-        possible_responses = [
-            "Prepare for unforseen consequences.",
-            "_This is where I get off..._",
-            "Yeah, when _is_ Half-Life 3 coming out, anyway?",
-            "Okay, so maybe I'm tinkering around with the Source engine and making a Half-Life fan game. So what?",
-            "Rise and smell the ashes...",
-            "Can we not talk about this? Please?"
-        ]
-
-    elif "Monika" in statement or "biggest fan" in statement or "fan" in statement:
-        possible_responses = [
-            "Well, I certainly never expected for Monika to like me so much...",
-            "It's both Monika and TZKU that like me. A lot.",
-            "The fan situation's a bit complicated, ehehe~",
-            "I have many fans..."
-        ]
-
-    elif "Where's Boris?" in statement:
-        possible_responses = [
-            "https://cdn.discordapp.com/attachments/481922610742165514/482312409831309315/Alice_Maymay_2.png ",
-            "Hippity hoppity, your Boris is now my property. That's how it goes, right?\nBut seriously, though, I have no clue.",
-            ":shrug:",
-            "W-why are you asking me?",
-            "Not here, not there, not anywhere to my knowledge!",
-            "Boris out, Eugene in!"
-        ]
-
-    elif "qt" in statement or "You're a Qt" in statement or "Qt" in statement or "you" and "qt" in statement:
-        possible_responses = [
-            "I NIOT QT!",
-            "NO\nBAKA",
-            "No. Eugene more Qt than me.",
-            "no u"
-        ]
-
-    elif "Who's Eugene?" in statement:
-        possible_responses = [
-            "Silly, Eugene's my stuffed mastodon!",
-            ":elephant:",
-            "B-but I thought you knew... :pensive:",
-            "What, am I not allowed to have stuffed animals or something?",
-            "He's the _cutest_ little stuffed mastodon!",
-            "My cuddle buddy. .^_~"
-        ]
-
-    else:
-        possible_responses = [
-            "Sorry, I don't understand.",
-            "Uwaa~!",
-            "Sadly, I'm not as complete as you think. It'll take some time before I can answer all of your questions.",
-            "E-Eh?",
-            "Are you talking to me or to Eugene?"
-        ]
-    await bot.say(random.choice(possible_responses))
-
+# Run the bot. This needs the environment key BOT_KEY to run!
+# This is set through Heroku usually, but can also work for
+# regular servers. Go to https://discordapp.com/developers/ to
+# create your bot key.
 bot.run(os.environ.get('BOT_KEY'))
